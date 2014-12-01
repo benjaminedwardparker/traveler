02afcb7cbd63016ba8d181fcc24d62285371f869
Benjamin Parker <benjaminparker@gmail.com>
2014-11-30 22:25:17 -0600

    Add 1-week forecast

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,19 +3,28 @@ class PagesController < ApplicationController
     if params[:lat].present?
       @lat = params[:lat]
       @lng = params[:lng]
+      @placename = params[:city]
     else
       @lat = 29.76
       @lng = -95.38
+      @placename = "Houston"
     end
     remote = Songkickr::Remote.new(ENV["SONGKICK_API_KEY"])
     results = remote.events(location: "geo:#{@lat},#{@lng}", type: 'festival')
-    @venues = results.results
+    @venues = []
+    results.results.each do |result|
+      if result.location.lat.present?
+        @venues << result
+      end
+    end
+    # @venues = results.results
   end
 
   def visit
-    results = Geocoder.search(params[:city]["city"])
+    @city = params[:city]["city"]
+    results = Geocoder.search(@city)
     @lat = results.first.data["lat"].to_f.round(2)
     @lng = results.first.data["lon"].to_f.round(2)
-    redirect_to root_path({lat: @lat, lng: @lng})
+    redirect_to root_path({lat: @lat, lng: @lng, city: @city})
   end
 end
